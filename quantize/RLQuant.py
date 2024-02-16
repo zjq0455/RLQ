dec6f977f51961d339af8ee6424c1a350495d354
chao <2925482988@qq.com>
2024-02-16 10:10:18 +0800

    commit quantization file

---
+++ quantize/RLQuant.py
@@ -0,0 +1,291 @@
+import torch
+import torch.nn as nn
+from models.int_llama_layer import QuantLlamaDecoderLayer
+from models.int_opt_layer import QuantOPTDecoderLayer
+from quantize.int_linear import QuantLinear
+import auto_gptq.nn_modules.qlinear.qlinear_cuda as qlinear_cuda
+from contextlib import nullcontext
+import copy
+import math
+import utils
+import os
+import pdb
+import gc
+
+
+
+def get_named_linears(module):
+    return {name: m for name, m in module.named_modules() if isinstance(m, QuantLinear)}
+
+
+def RLQuant(
+    lm,
+    args,
+    dataloader,
+    act_scales,
+    act_shifts,
+    logger=None,
+):
+    logger.info("Starting ...")
+    
+    # move embedding layer and first layer to target device
+    model = lm.model
+    dev = lm.device
+    use_cache = model.config.use_cache
+    model.config.use_cache = False
+    is_llama = False
+    if "llama" in args.net.lower():
+        is_llama = True
+        layers = model.model.layers
+        model.model.embed_tokens = model.model.embed_tokens.to(dev)
+        model.model.norm = model.model.norm.to(dev)
+        DecoderLayer = QuantLlamaDecoderLayer
+        pairs = {
+            "q_proj":"qkv",
+            "o_proj":"out",
+            "up_proj":"fc1"
+        }
+        layer_name_prefix = "model.layers"
+    elif "opt" in args.net.lower():
+        layers = model.model.decoder.layers
+        print(layers)
+        model.model.decoder.embed_tokens = model.model.decoder.embed_tokens.to(dev)
+        model.model.decoder.embed_positions = model.model.decoder.embed_positions.to(dev)
+        if hasattr(model.model.decoder, "project_out") and model.model.decoder.project_out:
+            model.model.decoder.project_out = model.model.decoder.project_out.to(dev)
+        if hasattr(model.model.decoder, "project_in") and model.model.decoder.project_in:
+            model.model.decoder.project_in = model.model.decoder.project_in.to(dev)
+        DecoderLayer = QuantOPTDecoderLayer
+        pairs = {
+            "q_proj":"qkv",
+            "out_proj":"out",
+            "fc1":"fc1"
+        }
+        layer_name_prefix = "model.decoder.layers"
+    else:
+        raise ValueError("Only support for opt/llama/Llama-2 now")
+    
+    
+    layers[0] = layers[0].to(dev)
+    print("------------")
+    if args.deactive_amp and args.epochs>0:
+        dtype = torch.float
+        traincast = nullcontext
+    else:
+        dtype = torch.float16
+        traincast = torch.cuda.amp.autocast
+    inps = torch.zeros(
+        (args.nsamples, lm.seqlen, model.config.hidden_size), dtype=dtype, device=dev
+    )
+
+    cache = {"i": 0}
+    # catch the first layer input 
+    class Catcher(nn.Module):
+        def __init__(self, module):
+            super().__init__()
+            self.module = module
+            self.is_llama = False
+
+        def forward(self, inp, **kwargs):
+            inps[cache["i"]] = inp
+            cache["i"] += 1
+            cache["attention_mask"] = kwargs["attention_mask"]
+            if self.is_llama:
+                cache["position_ids"] = kwargs["position_ids"]
+            raise ValueError
+
+    layers[0] = Catcher(layers[0])
+    layers[0].is_llama = is_llama
+
+    with torch.no_grad():
+        for batch in dataloader:
+            if cache["i"] >= args.nsamples:
+                break
+            try:
+                model(batch[0].to(dev))
+            except ValueError:
+                pass
+    
+    # move embedding layer and first layer to cpu
+    layers[0] = layers[0].module
+    layers[0] = layers[0].cpu()
+    if "llama" in args.net.lower():
+        model.model.embed_tokens = model.model.embed_tokens.cpu()
+        model.model.norm = model.model.norm.cpu()
+    elif "opt" in args.net.lower():
+        model.model.decoder.embed_tokens = model.model.decoder.embed_tokens.cpu()
+        model.model.decoder.embed_positions = model.model.decoder.embed_positions.cpu()
+        if hasattr(model.model.decoder, "project_out") and model.model.decoder.project_out:
+            model.model.decoder.project_out = model.model.decoder.project_out.cpu()
+        if hasattr(model.model.decoder, "project_in") and model.model.decoder.project_in:
+            model.model.decoder.project_in = model.model.decoder.project_in.cpu()
+    else:
+        raise ValueError("Only support for opt/llama/Llama-2 now")
+    torch.cuda.empty_cache()
+
+    
+    # same input of first layer for fp model and quant model
+    quant_inps = inps
+    fp_inps = copy.deepcopy(inps)   # take output of fp model as input
+    fp_inps_2 = copy.deepcopy(inps) if args.aug_loss else None # take output of quantization model as input
+    
+
+    attention_mask = cache["attention_mask"]
+    attention_mask_batch = attention_mask.repeat(args.batch_size,1,1,1) if args.deactive_amp else attention_mask.repeat(args.batch_size,1,1,1).float()
+    loss_func = torch.nn.MSELoss()
+    # loss_func = torch.nn.L1Loss()
+    if is_llama:
+        position_ids = cache["position_ids"]
+    else:
+        position_ids = None
+    cossim = nn.CosineSimilarity(dim=2)
+
+    if args.resume:
+        rlq_parameters = torch.load(args.resume)
+    else:
+        rlq_parameters = {}
+    
+    for i in range(len(layers)):
+        logger.info(f"=== Start quantize layer {i} ===")
+        print(len(layers))
+        
+        layer = layers[i].to(dev)
+        qlayer = DecoderLayer(lm.model.config, layer, args)
+        qlayer = qlayer.to(dev)
+
+        
+        # obtain output of full-precision model
+        qlayer.set_quant_state(weight_quant=False, act_quant=False)
+        if args.epochs > 0:
+            with torch.no_grad():
+                with torch.cuda.amp.autocast():
+                    for j in range(args.nsamples):
+                        fp_inps[j] = qlayer(fp_inps[j].unsqueeze(0), attention_mask=attention_mask,position_ids=position_ids)[0]
+                        if args.aug_loss:
+                            fp_inps_2[j] = qlayer(quant_inps[j].unsqueeze(0), attention_mask=attention_mask,position_ids=position_ids)[0]
+
+        # init smooth parameters
+        qlayer.set_quant_state(weight_quant=False, act_quant=True)  # weight will be manually quantized before forward
+        qlayer.let = args.let
+        use_shift = True 
+        # if is_llama and args.abits == 16:
+        #     use_shift = False                   # deactivate channel-wise shifting for llama weight-
+        # use_shift = True if args.abits < 16 else False   # only activate per-channel shifting when weight-activation quantization
+        
+        if args.let:
+            # init channel-wise scaling and shift
+            qlayer.register_parameter("qkt_smooth_scale",torch.nn.Parameter(torch.ones(layer.self_attn.q_proj.out_features,device=dev, dtype=dtype)))
+            for name,module in qlayer.named_modules():
+                if isinstance(module, QuantLinear):
+                    for key in pairs.keys():
+                        if key in name:
+                            act = act_scales[f"{layer_name_prefix}.{i}.{name}"].to(device=dev, dtype=dtype).clamp(min=1e-5) #4096
+
+                            scale = (act/torch.log2(2+act)).clamp(min=1e-5) #weight
+                            if use_shift and not is_llama:
+                                shift = act_shifts[f"{layer_name_prefix}.{i}.{name}"].to(device=dev, dtype=dtype)
+                            else:
+                                # shift = torch.zeros_like(scale)
+                                shift = torch.zeros_like(act)
+                            qlayer.register_parameter(f"{pairs[key]}_smooth_shift",torch.nn.Parameter(shift))
+                            qlayer.register_parameter(f"{pairs[key]}_smooth_scale",torch.nn.Parameter(scale))
+        
+        if args.resume:
+            qlayer.load_state_dict(rlq_parameters[i], strict=False)
+        
+        if args.epochs > 0:
+            with torch.no_grad():
+                qlayer.float()      # required for AMP training
+            # create optimizer
+            optimizer = torch.optim.AdamW(
+                [{"params":qlayer.let_parameters(use_shift),"lr":args.let_lr}, {"params":qlayer.lwc_parameters(),"lr":args.lwc_lr}],weight_decay=args.wd)
+            loss_scaler = utils.NativeScalerWithGradNormCount()
+                   
+            for epochs in range(args.epochs):
+                loss_list = []
+                norm_list = []
+                for j in range(args.nsamples//args.batch_size):    
+                    index = j * args.batch_size
+                    # obtain output of quantization model
+                    with traincast():
+                        qlayer.smooth_and_quant_temporary()
+                        quant_out = qlayer(quant_inps[index:index+args.batch_size,], attention_mask=attention_mask_batch,position_ids=position_ids)[0]
+                        loss = loss_func(fp_inps[index:index+args.batch_size,], quant_out)
+                        # loss2 = loss_func(ones_ops[index:index+args.batch_size,], quant_out_ones)
+                        cos = cossim(quant_out,fp_inps[index:index+args.batch_size,]).mean().abs()
+                        loss -= torch.log(cos)
+                        if args.aug_loss:
+                            loss += loss_func(fp_inps_2[index:index+args.batch_size,], quant_out)
+                    if not math.isfinite(loss.item()):
+                        logger.info("Loss is NAN, stopping training")
+                        # quant_out = qlayer(quant_inps[index:index+args.batch_size,], attention_mask=attention_mask_batch,position_ids=position_ids)[0]
+                        pdb.set_trace()
+                        
+                    loss_list.append(loss.data)
+                    optimizer.zero_grad()
+                    norm = loss_scaler(loss, optimizer,parameters=qlayer.rlq_parameters(use_shift))
+                    norm_list.append(norm.data)
+
+                loss_mean = torch.stack(loss_list).mean()
+                norm_mean = torch.stack(norm_list).mean()
+                logger.info(f"layer {i} iter {epochs} loss:{loss_mean} norm:{norm_mean} max memory_allocated {torch.cuda.max_memory_allocated(lm._device) / 1024**2} ")
+            qlayer.clear_temp_variable()
+            del optimizer
+        
+        
+        # real smooth and quantization
+        qlayer.smooth_and_quant_inplace()       
+        if args.epochs>0:
+            # update input of quantization model
+            with torch.no_grad():
+                with torch.cuda.amp.autocast():
+                    for j in range(args.nsamples):
+                        quant_inps[j] = qlayer(quant_inps[j].unsqueeze(0), attention_mask=attention_mask,position_ids=position_ids)[0]
+            qlayer.register_scales_and_zeros()
+            qlayer.half()
+            layers[i] = qlayer.to("cpu")
+            rlq_parameters[i] = qlayer.rlq_state_dict()
+            torch.save(rlq_parameters, os.path.join(args.output_dir, f"rlq_parameters.pth"))
+        else:
+            qlayer.register_scales_and_zeros()
+            qlayer.half()
+            layers[i] = qlayer.to("cpu")    
+
+        
+        if args.real_quant:
+            named_linears = get_named_linears(qlayer)
+            for name, module in named_linears.items():
+                scales = module.weight_quantizer.scales
+                zeros = module.weight_quantizer.zeros
+                group_size = module.weight_quantizer.group_size
+                dim0 = module.weight.shape[0]
+                scales = scales.view(dim0,-1)
+                zeros = zeros.view(dim0,-1)
+                q_linear = qlinear_cuda.QuantLinear(args.wbits, group_size, module.in_features,module.out_features,not module.bias is None)
+                q_linear.pack(module.float().cpu(),  scales.float().cpu(), zeros.float().cpu())
+                
+                levels = name.split('.')
+                if len(levels) > 1:
+                    mod_ = qlayer
+                    for l_idx in range(len(levels)-1):
+                        if levels[l_idx].isdigit():
+                            mod_ = mod_[int(levels[l_idx])]
+                        else:
+                            mod_ = getattr(mod_, levels[l_idx])
+                    setattr(mod_, levels[-1], q_linear)
+                else:
+                    setattr(qlayer, name, q_linear)        
+                del module        
+        
+        del layer
+        torch.cuda.empty_cache()
+        
+
+    del inps
+    del quant_inps
+    del fp_inps
+    del fp_inps_2
+    torch.cuda.empty_cache()
+    gc.collect()                    
+    model.config.use_cache = use_cache
+    return model
